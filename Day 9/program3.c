704baa0f5dcc710e914e25e377d79e2fedd38b4f
Divyanshu21708 <25110cn402@glbitm.ac.in>
2026-06-29 22:27:51 +0530

    Add files via upload

---
+++ Day 9/program3.c
@@ -0,0 +1,16 @@
+/* Program to print repeated character pattern */
+
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%c", 'A' + (i - 1));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
